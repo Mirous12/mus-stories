d9aa7fac320380e7f32bb0b5524c8b00ba5d4eac
unknown <zloyloki00@gmail.com>
2022-02-22 13:07:32 +0300

    Test cummit

--- Scripts/ButtonStart.gd
+++ Scripts/ButtonStart.gd
@@ -10,9 +10,7 @@ extends Button
 func _ready():
 	pass;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
 
 
 func _on_Button_pressed():
